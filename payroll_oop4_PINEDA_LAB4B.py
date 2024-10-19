c56c768df348996a058438615bc91e75b126f029
ceski <2023-2-02517@lpunetwork.edu.ph>
2024-10-19 08:39:37 +0800

    oct 18 24

--- payroll_oop4_PINEDA_LAB4B.py
+++ payroll_oop4_PINEDA_LAB4B.py
@@ -3,16 +3,16 @@
 
 import payroll_oop2_PINEDA_LAB4B
 
-#instantiate payroll_oop2.py and place it inside the employee payroll. Note: employee payroll is changeable as you wish
+#Instantiate payroll_oop2.py and place it inside the employee payroll. Note: Employee payroll is changeable as you wish
 employee_payroll = payroll_oop2_PINEDA_LAB4B.Employee_Salary()
 emp_rate_per_hour = float(input("Enter value for rate per our: "))
 emp_num_of_absences = int(input("Enter value for number of absences: "))
 tardiness_hours = int(input("Enter number of tardiness: "))
 
-#accessing the codes under attribute get absences deduction inside the Employee Salary class.
+#Accessing the codes under the attribute get absences deduction inside the Employee Salary class.
 absences_deduction = employee_payroll.get_absences_deduction(emp_rate_per_hour, emp_num_of_absences)
 tardiness_deduction = employee_payroll.get_tardiness_deduction(emp_rate_per_hour, tardiness_hours)
 
-#formula to compute partial deduction of an employee
+#Formula to compute the partial deduction of an employee
 partial_deduction = absences_deduction + tardiness_deduction
 print("Employee partial deduction is: %.2f" % partial_deduction)
